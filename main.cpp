14cbbc13c3de8cb9fd75d88a02fcf9ea7f6dc32e
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-08-27 12:57:43 +0530

    Added new function

--- main.cpp
+++ main.cpp
@@ -3,42 +3,50 @@
 #include "telco.h"
 
 
+
+
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
 {
+
     TelCoColorCoder::ColorPair colorPair =
     TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
+
 }
 
+
+
 void testPairToNumber(
+
     TelCoColorCoder::MajorColor major,
     TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
+
 {
+
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
+
 }
+
 
 //new function added that gives all color
 //code with the pair numbers
 
-void printAllCodesAndColors()
-{
-    for (int pairNumber = 1; pairNumber < 26; pairNumber++) {
-        TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-        std::cout  << pairNumber << ":  " <<  colorPair.ToString() << std::endl;
-    }
-}
+void insertColorCodes();
+void printColorCodes();
 
 
-int main(){
 
-    printAllCodesAndColors();
+
+int main() {
+    insertColorCodes();
+    //printColorCodes();
 
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
@@ -47,4 +55,5 @@ int main(){
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     return 0;
+
 }
