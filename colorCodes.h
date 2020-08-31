245cf369ba4afbfa4a2beea5f839590df1aca87a
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-08-31 22:13:03 +0530

    Return String

--- colorCodes.h
+++ colorCodes.h
@@ -1,47 +1,23 @@
-#include<map>
+#include<sstream>
+using namespace std;
+
 namespace colorCodes
 {
-	std::map<int, std::string> TheColorCodes;
+	string getColorCodes(int numberOfMajorColors, int numberOfMinorColors) {
 
-	void insertColorCodes()
-	{
-		TheColorCodes[1] = { "White Blue" };
-		TheColorCodes[2] = { "White Orange" };
-		TheColorCodes[3] = { "White Green" };
-		TheColorCodes[4] = { "White Brown" };
-		TheColorCodes[5] = { "White Slate" };
-		TheColorCodes[6] = { "Red Blue" };
-		TheColorCodes[7] = { "Red Orange" };
-		TheColorCodes[8] = { "Red Green" };
-		TheColorCodes[9] = { "Red Brown" };
-		TheColorCodes[10] = { "Red Slate" };
-		TheColorCodes[11] = { "Black Blue" };
-		TheColorCodes[12] = { "Black Orange" };
-		TheColorCodes[13] = { "Black Green" };
-		TheColorCodes[14] = { "Black Brown" };
-		TheColorCodes[15] = { "Black Slate" };
-		TheColorCodes[16] = { "Yellow Blue" };
-		TheColorCodes[17] = { "Yellow Orange" };
-		TheColorCodes[18] = { "Yellow Green" };
-		TheColorCodes[19] = { "Yellow Brown" };
-		TheColorCodes[20] = { "Yellow Slate" };
-		TheColorCodes[21] = { "Violet Blue" };
-		TheColorCodes[22] = { "Violet Orange" };
-		TheColorCodes[23] = { "Violet Green" };
-		TheColorCodes[24] = { "Violet Brown" };
-		TheColorCodes[25] = { "Violet Slate" };
-
-
-
-	}
-
-	void printColorCodes()
-	{
-		for (auto it = TheColorCodes.begin(); it != TheColorCodes.end(); ++it)
-		{
-
-			std::cout << it->first << "   " << it->second << std::endl;
-
+		int outer = 0, inner = 0, pairNumber = 1;
+		string AllColorCodes = "";
+		for (outer = 0; outer < numberOfMajorColors; outer++) {
+			for (inner = 0; inner < numberOfMinorColors; inner++) {
+				TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+				ostringstream ss;
+				ss << pairNumber;
+				string temp = ss.str() + " : " + colorPair.ToString();
+				temp += "\n";
+				AllColorCodes += temp;
+				pairNumber++;
+			}
 		}
+		return AllColorCodes;
 	}
 }
